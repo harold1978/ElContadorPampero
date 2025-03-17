ba5be0df394217d1a9438a4955a5a6bb00ca1d74
HAROLD MUÑOZ <36018526+harold1978@users.noreply.github.com>
2025-03-17 13:48:09 -0600

    conexion

--- ElContadorPampero/Data/ElContador2025V2Context.cs
+++ ElContadorPampero/Data/ElContador2025V2Context.cs
@@ -34,10 +34,12 @@ public partial class ElContador2025V2Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=HEMM2024GAMING\\SQLEXPRESS;Initial Catalog=ElContador2025V2;Integrated Security=True;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=(local)\\SQLEXPRESS;Initial Catalog=ElContador2025V2;Integrated Security=True;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.UseCollation("Modern_Spanish_CI_AS");
+
         modelBuilder.Entity<AsientoContable>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__AsientoC__3214EC07CEA635D2");
